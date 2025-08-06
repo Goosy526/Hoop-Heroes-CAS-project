242c4aed0803b78f2c8c8c529daeba9d4475237c
Goosy526 <gaspard.hanrahan7@gmail.com>
2025-08-06 19:27:14 +1000

    Project Start

---
+++ Assets/player2Movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2Movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float Move;
+
+    public float Speed;
+    public float Jump;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxisRaw("Horizontal(arrows)");
+
+        rb.linearVelocity = new Vector2(Move * Speed, rb.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump(arrows)"))
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, Jump * 10));
+        }
+    }
+}
